f6f879cb8ab1817ba9378f12186ea12f2abd7b4f
Your Name <you@example.com>
2022-07-28 18:45:17 +0700

    bbase authentication cÃushis

---
+++ src/main/java/com/example/lab1/AuthFilter.java
@@ -0,0 +1,30 @@
+package com.example.lab1;
+
+
+import javax.servlet.*;
+import javax.servlet.annotation.WebFilter;
+import java.io.IOException;
+
+@WebFilter("/*")
+public class AuthFilter implements Filter {
+
+    @Override
+    public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
+        // logic handle authentication cushis
+        // 1. get cookie, nếu null thì redirect sang trang login của sso
+        // 2. nếu get cookie thành công thì gọi api http post để lấy user info,
+        // nếu get thành công thì trả về trang home hoặc thực thi các api tiếp theo,
+        // còn không thì redirect sang trang login của sso
+
+        request.getRequestDispatcher("home.xhtml").forward(request, response);
+    }
+
+    @Override
+    public void init(FilterConfig filterConfig) throws ServletException {
+    }
+
+    @Override
+    public void destroy() {
+    }
+
+}
---
+++ src/main/java/com/example/lab1/Home.java
@@ -0,0 +1,19 @@
+package com.example.lab1;
+
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.RequestScoped;
+import java.io.Serializable;
+
+@ManagedBean(name = "home", eager = true)
+@RequestScoped
+public class Home implements Serializable {
+    private static final long serialVersionUID = 1L;
+
+    public Home() {
+    }
+
+    public String getMessage() {
+        return "Welcome Cushis";
+    }
+
+}
